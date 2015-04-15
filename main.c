0d4b48b75e1c9f6e6e0ac439f88583a8e6802ad2
dobesvac <dobes.va@gmail.com>
2015-04-15 22:35:22 +0200

    pridani barvy

--- main.c
+++ main.c
@@ -4,12 +4,16 @@ struct mdl_rectangle {
  uint32_t y;
  uint32_t width;
  uint32_t height;
+ uint32_t color_fill;
+ uint32_t color_line;
 };
 
 struct mdl_circle {
  uint32_t x; /* Centre */
  uint32_t y; /* Centre */
  uint32_t radius;
+ uint32_t color_fill;
+ uint32_t color_line;
 }
 
 void mdl_draw_rectangle(struct mdl_rectangle *rect, int flags)
